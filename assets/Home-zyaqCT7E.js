13bd2c5fb7f831943ad65b60f14aece98acb4c90
Anton Yanaky <171488152+AntonYanaky@users.noreply.github.com>
2025-07-19 18:45:55 -0400

    Updates

---
+++ assets/Home-zyaqCT7E.js
@@ -0,0 +1,7 @@
+import{j as r}from"./three-vendor-CnkMCase.js";import{c as a}from"./index-veoxvMDR.js";import{H as o,F as s}from"./Footer-BjkErQpW.js";import"./react-router-B7kevx0P.js";import"./react-vendor-1zw1pNgy.js";const c="/assets/carlandscape-CDjw6u5e.webp",i="/assets/carportrait-wC5FLTdy.webp";function u(){const t=a.c(1);let e;return t[0]===Symbol.for("react.memo_cache_sentinel")?(e=r.jsxs("div",{className:"bg-black min-h-screen flex flex-col",style:{backgroundImage:`url(${i})`,backgroundSize:"cover",backgroundPosition:"center",backgroundRepeat:"no-repeat"},children:[r.jsx("style",{jsx:!0,children:`
+        @media (min-width: 768px) {
+          div {
+            background-image: url(${c}) !important;
+          }
+        }
+      `}),r.jsx(o,{}),r.jsx(s,{})]}),t[0]=e):e=t[0],e}export{u as default};
